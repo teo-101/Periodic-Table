1c8dda864c009e7eab61b93ef949cd9592e791d0
Teo <th3od0ry@gmail.com>
2023-04-12 13:39:06 +0300

    Font change

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const form = document.querySelector('form');
 
 form.addEventListener('click', (event) => {
-    const typeOfElem = document.querySelector('input[name="typeOfElem"]:checked');
-    const selectedType = typeOfElem ? typeOfElem.nextElementSibling.innerHTML : '';
+  const typeOfElem = document.querySelector('input[name="typeOfElem"]:checked');
+  const selectedType = typeOfElem ? typeOfElem.value : '';
 
-    const elements = document.getElementsByClassName('cell');
+  const elements = document.getElementsByClassName('cell');
 
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].classList.contains(selectedType)) {
-            elements[i].style.opacity = '1';
-        } else {
-            elements[i].style.opacity = '0.3';
-        }
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].classList.contains(selectedType)) {
+      elements[i].style.opacity = '1';
+    } else {
+      elements[i].style.opacity = '0.3';
     }
+  }
 });
