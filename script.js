79cdf865501f568e82b52f58fe53c9fe2bb34edd
Teo <th3od0ry@gmail.com>
2023-04-12 15:16:57 +0300

    Radioactive

--- script.js
+++ script.js
@@ -9,8 +9,15 @@ form.addEventListener('click', (event) => {
   for (let i = 0; i < elements.length; i++) {
     if (elements[i].classList.contains(selectedType)) {
       elements[i].style.opacity = '1';
+
+      if (elements[i].classList.contains('Radioactive') && selectedType === 'Radioactive') {
+        elements[i].style.backgroundColor = '#28df28';
+      } else {
+        elements[i].style.backgroundColor = '';
+      }
     } else {
       elements[i].style.opacity = '0.3';
+      elements[i].style.backgroundColor = '';
     }
   }
 });
